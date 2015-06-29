c7183dae6360f60c858507781cf213bb343e2282
Stjerndal <stjernd@kth.se>
2015-06-29 13:54:59 +0200

    Added car AI scene&dependencies.

---
+++ Assets/Scripts/Car/CarControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarControl : MonoBehaviour {
+
+	private float speedLimit = 0;
+	private Rigidbody myBody;
+
+	void Awake(){
+		myBody = GetComponent<Rigidbody>();
+		if(!myBody)
+			Debug.LogError("No rigidbody found on CarControl object!");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Go(float distance, float speedLimit) {
+
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
