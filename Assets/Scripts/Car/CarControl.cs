327e307576ae68e92398d6557e6579a0370bba1b
Stjerndal <stjernd@kth.se>
2015-06-29 20:03:20 +0200

    Go(), Stop(), distance checking etc functionality.

--- Assets/Scripts/Car/CarControl.cs
+++ Assets/Scripts/Car/CarControl.cs
@@ -6,26 +6,38 @@ public class CarControl : MonoBehaviour {
 	private float speedLimit = 0;
 	private Rigidbody myBody;
 	private float distanceTraveled = 0;
+	private float targetDistance = 0;
+	private Vector3 startPos;
 
 	void Awake(){
 		myBody = GetComponent<Rigidbody>();
 		if(!myBody)
 			Debug.LogError("No rigidbody found on CarControl object!");
+		startPos = transform.position;
 	}
 
 	// Use this for initialization
 	void Start () {
-		Go (10, 20, new Vector3(1,0,0));
+		Go (1000, 20, new Vector3(1,0,0));
 	}
 
 	void Go(float distance, float speedLimit, Vector3 direction) {
+		startPos = transform.position;
+		distanceTraveled = 0;
 		this.speedLimit = speedLimit;
+		targetDistance = distance;
 		myBody.velocity = speedLimit * direction;
 	}
 
+	void Stop() {
+		myBody.velocity = Vector3.zero;
+	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		distanceTraveled += (startPos - transform.position).magnitude;
+		if(distanceTraveled > targetDistance) {
+			Stop ();
+		}
 	}
 }
