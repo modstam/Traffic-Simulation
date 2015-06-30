8a4ee089b4affa449551f556de8debc68c03320b
modstam <modstam@live.com>
2015-06-30 14:09:00 +0200

    stugg

--- Assets/Scripts/Node/Network.cs
+++ Assets/Scripts/Node/Network.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using System.Collections;
+
 
 public class Network : MonoBehaviour {
 
+	[SerializeField]
+	public Node[] nodes; 
+
 	// Use this for initialization
 	void Start () {
-	
+		//nodes = new ArrayList();
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/Node/Node.cs
+++ Assets/Scripts/Node/Node.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Node : MonoBehaviour {
 
+
+	public Node[] neighbors;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +13,16 @@ public class Node : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+	}
+
+	void OnDrawGizmos(){
+		for(int i = 0; i < neighbors.Length; ++i){
+			if(neighbors[i] != null){
+				Vector3 start = this.transform.position;
+				Vector3 end = neighbors[i].transform.position;			
+				Gizmos.DrawLine(start ,end);
+			}
+		}
 	}
 }
